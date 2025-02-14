3d0afea4da9cc1ee865072348f70354546f511ff
LazyManish <mbhattarai900@gmail.com>
2025-02-14 11:48:21 +0545

    modified in logic of  double entry for same user

--- group/api/views.py
+++ group/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status,viewsets
 from rest_framework.permissions import IsAuthenticated
-from .serializers import GroupSerializer, MembershipSerializer
+from .serializers import GroupSerializer
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from group.models import Group,Membership
@@ -40,7 +40,7 @@ class GroupViewSets(viewsets.ModelViewSet):
                 group_id = group_id,
                 group_password = group_password
             )
-
+            
             Membership.objects.create(
                 group = group,
                 user = request.user,
@@ -52,29 +52,47 @@ class GroupViewSets(viewsets.ModelViewSet):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
-    @action(detail=True, methods =['post'])
+    @action(detail=True, methods =['post'], permission_classes =[IsAuthenticated])
     def invite(self,request, pk=None):
 
-
+        try:
             group = self.get_object()
+            group_id = request.data.get('group_id')
             group_password = request.data.get('group_password')
+            username = self.request.user
+
+            if group_id != group.group_id:
+                return Response({"message":"ID didn't match"}, status=status.HTTP_400_BAD_REQUEST)
 
             if group_password != group.group_password:
                 return Response({"message":"Password didn't match"}, status=status.HTTP_400_BAD_REQUEST)
             
             try:
-                user = User.objects.get(username = request.data['username'])
+                user = User.objects.get(username = username)
             
             except User.DoesNotExist:
                 return Response({"message":"User Doesnot Exist."}, status=status.HTTP_404_NOT_FOUND)
             
-            Membership.objects.create(
-                group = group,
-                user = user,
-                role = 'member'
-            )
+            try:
+                if Membership.objects.filter(group=group, user=user).exists():
+                    return Response({"message":f"username {user.username} is already a member of this group."})
+                else:
+            
+                    Membership.objects.create(
+                        group = group,
+                        user = user,
+                        role = 'member'
+                    )
 
-            return Response({"message":f"{user.username} added to the group."}, status=status.HTTP_200_OK)
+                    return Response({"message":f"{user.username} added to the group."}, status=status.HTTP_200_OK)
+                
+            except Exception as e:
+                return Response({"error": str(e)}, status= status.HTTP_400_BAD_REQUEST)
+            
+            
+        except Exception as e:
+                return Response({"error": str(e)}, status= status.HTTP_400_BAD_REQUEST)
+            
 
         
 
